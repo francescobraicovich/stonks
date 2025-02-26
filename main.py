524fbb9f5b3ae0e8f78f0168d1d51500c2cce61e
francescobraicovich <francesco.braicovich@gmail.com>
2025-02-26 21:03:50 +0100

    Add command-line argument for configuration file path in the Reddit data ingestion pipeline

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 
 import logging
 import os
+import argparse
+
 from pipelines.reddit_price_pipeline import run_pipeline
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -9,6 +12,16 @@ logging.basicConfig(
 )
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Reddit Data Ingestion Pipeline")
+    parser.add_argument(
+        "--config", 
+        type=str, 
+        default="config.yaml", 
+        help="Path to configuration YAML file"
+    )
+    args = parser.parse_args()
+
     config_path = os.getenv("CONFIG_PATH", "config/config.yml")
 
     run_pipeline(config_path)
